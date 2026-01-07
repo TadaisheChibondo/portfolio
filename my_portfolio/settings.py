dc81f9b214b5e7b53f7492d1eba9a8ec1e0de0a5
Tadaishe Chibondo <tadaishechibondo@gmail.com>
2026-01-07 10:26:14 +0200

    Fix Allowed Hosts

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-@=wvx*#mn^%9d(bxg7p_9to^rv$5rnei68^2)h+bi58o-xy6=y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
